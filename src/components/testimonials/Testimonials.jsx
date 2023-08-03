803f42f00707c2fa28d82c7ca8754d87e47979a0
Carolina Silva <carolinasilvagc08@gmail.com>
2023-08-03 17:51:15 -0300

    Finalizando o componente Testimonials com o carrossel de recomendações

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
 import './testimonials.css'
+import testimonialsArray from './testimonialData'
+
+// Core modules imports are same as usual
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+
+// Styles must use direct files imports
+import 'swiper/css';
+import  'swiper/css/pagination';
 
 const Testimonials = () => {
   return (
     <section id='testimonials'>
-      Testimonials
+      <h5>Palavras de quem já trabalhou comigo</h5>
+      <h2>Recomendações do LinkedIn</h2>
+
+      <Swiper className='container testimonials__container'
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
+        {
+          testimonialsArray.map(({avatar, name, review}, index) => {
+            return (
+              <SwiperSlide className='testimonial' key={index}>
+                <div className='reviewer__avatar'>
+                  <img src={avatar} alt={name}/>
+                </div>
+                <h5 className='reviewer'>{name}</h5>
+                <small className='review'>{review}</small>
+              </SwiperSlide>
+            )
+          })
+        }
+      </Swiper>
     </section>
   )
 }
---
+++ src/components/testimonials/testimonialData.js
@@ -0,0 +1,23 @@
+import AVTR1 from '../../assets/rafael-parigi.jpeg'
+import AVTR2 from '../../assets/marina-rozas.jpeg'
+import AVTR3 from '../../assets/quelvin-machado.jpg'
+
+const testimonialsArray = [
+  {
+    avatar: AVTR1,
+    name: 'Rafael Parigi - Coordenador de Marketing',
+    review:  'Eu trabalhei com a Carol por mais de um ano e isso me permitiu testemunhar o crescimento profissional dela. Graças ao seu dinamismo e criatividade, ela sempre propôs ideias inovadoras e atrativas. Ela é conectada com as tendências, muito inteligente, comunicativa e uma excelente pessoa para se ter por perto!'
+  },
+  {
+    avatar: AVTR2,
+    name: 'Marina Rozas - Engenheira de Software',
+    review: 'A Carol é uma excelente profissional, que trabalha incansavelmente para conseguir dar conta das prioridades do time, sem nunca perdê-las de vista. É também uma pessoa inspiradora, team-player, daquelas que transformam o ambiente e deixam o time 200% mais leve e gostoso de trabalhar.'
+  },
+  {
+    avatar: AVTR3,
+    name: 'Quelvin Machado - Desenvolvedor Front-End',
+    review: 'Em apenas um projeto feito em conjunto com a Carolina, foi o suficiente para ver o quão qualificada ela já se encontra. Durante todos os dias de projeto, ela se manteve em posição de liderança, levando o time às boas praticas de mercado, ajudando a manter a organização e padronização das ações (commit, branch, push, etc). Se eu tiver a sorte de ser liderado por ela em outro projeto, sei que estarei em boas mãos.'
+  }
+]
+
+export default testimonialsArray;
