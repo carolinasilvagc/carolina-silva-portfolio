53d84ce71738993c772ca9837c6205c88badeb8f
Carolina Silva <carolinasilvagc08@gmail.com>
2023-08-03 18:54:25 -0300

    Acrescentando um menu no componente Nav para levar ao portfólio de projetos

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,6 +5,7 @@ import {AiOutlineUser} from 'react-icons/ai'
 import {BiBook} from 'react-icons/bi'
 import {BiMessageDetail} from 'react-icons/bi'
 import {useState} from 'react'
+import {AiOutlineFolderOpen} from 'react-icons/ai'
 
 const Nav = () => {
   const [activeNav, setActiveNav] = useState('#')
@@ -13,6 +14,7 @@ const Nav = () => {
       <a href='#' onClick={(() => setActiveNav('#'))} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
       <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
       <a href='#experience' onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook /></a>
+      <a href='#portfolio' onClick={(() => setActiveNav('#portfolio'))} className={activeNav === '#portfolio' ? 'active' : ''}><AiOutlineFolderOpen /></a>
       <a href='#contact' onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageDetail /></a>
     </nav>
   )
