36624219ce299b00df75a559580780f844f389c4
Carolina Silva <carolinasilvagc08@gmail.com>
2023-08-03 21:57:05 -0300

    Finalizando o componente Footer. Também fiz uma pequena alteração de cor na paleta geral e na navbar

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
 import './footer.css'
+import {BsInstagram} from 'react-icons/bs'
+import {BsGithub} from 'react-icons/bs'
+import {BsLinkedin} from 'react-icons/bs'
 
 const Footer = () => {
   return (
-    <div>
-      <p>Footer</p>
-    </div>
+    <footer>
+      <a href='#' className='footer__logo'>LOLLACODES</a>
+      <ul className='permalinks'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#about'>Sobre mim</a></li>
+        <li><a href='#experience'>Experiência</a></li>
+        <li><a href='#portfolio'>Portfólio</a></li>
+        <li><a href='#testimonials'>Recomendações</a></li>
+      </ul>
+
+      <div className='footer__socials'>
+        <a href='https://www.instagram.com/lollacodes'><BsInstagram /></a>
+        <a href='https://www.github.com/carolinasilvagc'><BsGithub /></a>
+        <a href='https://www.linkedin.com/in/carolinasilvagc'><BsLinkedin /></a>
+      </div>
+
+      <div className='footer__copyright'>
+        <small>&copy; Página criada por Carolina Silva. Todos os direitos reservados.</small>
+      </div>
+    </footer>
   )
 }
 
